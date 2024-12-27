37ca67549f14e05e5deed484a95afe7fb21ad363
Julia Stephan <julia-stephan@hotmail.de>
2024-12-27 11:19:27 +0100

    refactor: changes admin url

--- job_data_hub/urls.py
+++ job_data_hub/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path(settings.ADMIN_URL, admin.site.urls),
     path("", include("job_interface.urls")),
     path("", include("job_db.urls")),
 ]
